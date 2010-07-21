193e94cc3bcf35d648b05d8f4f9f8e4bbc9301a1
Erik Spaan <erik.spaan@gmail.com>
2010-07-21 19:17:35 +0000

    fixes #113, now possible to have 2 content pages on one page. context menu takes into account pageid (in 3x and 400 branch)
    git-svn-id: file:///home/trac/svn/content/branches@470 709b8922-1eb5-43c7-a659-79efe9a4c303

--- Content-3.x/pnjavascript/ajax.js
+++ Content-3.x/pnjavascript/ajax.js
@@ -135,9 +135,9 @@ content.pageInfo = {};
 
 content.pageInfo.clearTimer = null;
 
-content.pageInfo.toggle = function()
+content.pageInfo.toggle = function(id)
 {
-  $('contentPageInfo').toggle();
+  $('contentPageInfo-'+id).toggle();
   return false;
 }
 
