87c684059172e4388e5f60b9eb4d4d319b9ef2b6
Sven Strickroth <email@cs-ware.de>
2010-12-21 22:46:53 +0100

    allow to filter for parent page

--- src/modules/Content/pnpageapi.php
+++ src/modules/Content/pnpageapi.php
@@ -280,6 +280,10 @@ function contentGetPageListRestrictions($filter, &$restrictions, &$join)
         $restrictions[] = $where;
     }
 
+    if (!empty($filter['parentId'])) {
+        $restrictions[] = " $pageColumn[parentPageId]= ".(int)$filter['parentId'];
+    }
+
     if (isset($filter['expandedPageIds']) && is_array($filter['expandedPageIds'])) {
         $pageIdStr = '-1';
         foreach (array_keys($filter['expandedPageIds']) as $pageId)
