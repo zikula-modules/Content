48b9663b6b75745a51c77b1c8a4e26dc3712939a
Erik Spaan <erik.spaan@gmail.com>
2011-01-08 14:08:54 +0100

    fixed bug in getPages where IsInMenu was not filled correctly (content32x)

--- src/modules/Content/pnpageapi.php
+++ src/modules/Content/pnpageapi.php
@@ -162,7 +162,7 @@ $orderBy";
         // create page variables that represent the Online and Menu status, much like the old db fields
         $now = DateUtil::getDatetime();
         $p['isOnline'] = $p['active'] && (DateUtil::getDatetimeDiff_AsField($p['activeFrom'], $now, 6) >= 0 || $p['activeFrom'] == null) && (DateUtil::getDatetimeDiff_AsField($p['activeTo'], $now, 6) < 0 || $p['activeTo'] == null);
-        $p['isInMenu'] = $p['isActive'] && $p['inMenu'];
+        $p['isInMenu'] = $p['isOnline'] && $p['inMenu'];
 
         if ($includeContent) {
             $content = pnModAPIFunc('content', 'content', 'getPageContent', array('pageId' => $p['id'], 'editing' => $editing, 'translate' => $translate));
