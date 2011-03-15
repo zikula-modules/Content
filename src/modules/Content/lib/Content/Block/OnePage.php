a44089d2ec7ad8223e6e0dd5328c0e1f76a81b4a
Craig Heydenburg <craigh@mac.com>
2011-03-15 16:17:12 -0400

    add OnePage block provided by nikp. closes #193 and closes #114. code changed to Zikula 1.3 standards. Code refs a 'onepage.tpl' template that doesn't seem to exist, but code functions as is, so left as is.

---
+++ src/modules/Content/lib/Content/Block/OnePage.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Content
+ *
+ * @copyright (C) 2007-2010, Content Development Team
+ * @link http://code.zikula.org/content
+ * @license See license.txt
+ * @author N.Petkov, based on block menu.php
+ */
+class Content_Block_OnePage extends Zikula_Controller_Block
+{
+
+    public function init()
+    {
+        // Security
+        SecurityUtil::registerPermissionSchema('Content:OnePageBlock:', 'Block title::');
+    }
+
+    public function info()
+    {
+        return array('module'          => 'Content',
+                     'text_type'       => $this->__('Content onepage'),
+                     'text_type_long'  => $this->__('Content onepage block'),
+                     'allow_multiple'  => true,
+                     'form_content'    => false,
+                     'form_refresh'    => false,
+                     'show_preview'    => true,
+                     'admin_tableless' => true);
+    }
+
+    public function display($blockinfo)
+    {
+        // security check
+        if (!SecurityUtil::checkPermission('Content:OnePageBlock:', "$blockinfo[title]::", ACCESS_READ)) {
+            return;
+        }
+
+        // Break out options from our content field
+        $vars = BlockUtil::varsFromContent($blockinfo['content']);
+        // --- Setting of the Defaults
+        if (!isset($vars['usecaching'])) {
+            $vars['usecaching'] = true;
+        }
+        if (!isset($vars['root'])) {
+            $vars['root'] = 0;
+        }
+
+        if ($vars['usecaching']) {
+            $cacheId = 'onepage|' . $blockinfo['title'] . '|' . ZLanguage::getLanguageCode();
+        } else {
+            $cacheId = null;
+        }
+        if (!$vars['usecaching'] || ($vars['usecaching'] && !$this->view->is_cached('block/onepage.tpl', $cacheId))) {
+            if ($vars['root'] > 0) {
+                $blockinfo['content'] = ModUtil::func('content', 'user', 'view', array('pid' => $vars['root']));
+            }
+        }
+
+        return BlockUtil::themeBlock($blockinfo);
+    }
+
+    public function modify($blockinfo)
+    {
+        $vars = BlockUtil::varsFromContent($blockinfo['content']);
+        if (!isset($vars['usecaching'])) {
+            $vars['usecaching'] = true;
+        }
+
+        $this->view->assign($vars);
+
+        $pages = ModUtil::apiFunc('content', 'page', 'getPages', array(
+            'makeTree' => false,
+            'orderBy' => 'setLeft',
+            'includeContent' => false));
+        $pidItems = array();
+        $pidItems[] = array('text' => $this->__('Select a page'), 'value' => "0");
+
+        foreach ($pages as $page) {
+            $pidItems[] = array('text' => str_repeat('+', $page['level']) . " " . $page['title'], 'value' => $page['id']);
+        }
+        $this->view->assign('pidItems', $pidItems);
+
+        return $this->view->fetch('block/onepage_modify.tpl');
+    }
+
+    function update($blockinfo)
+    {
+        $vars = BlockUtil::varsFromContent($blockinfo['content']);
+        $vars['root'] = FormUtil::getPassedValue('root', 0, 'POST');
+        $vars['usecaching'] = (bool)FormUtil::getPassedValue('usecaching', false, 'POST');
+
+        $blockinfo['content'] = BlockUtil::varsToContent($vars);
+
+        // clear the block cache
+        $this->view->clear_cache('block/onepage.tpl');
+
+        return $blockinfo;
+    }
+}
---
+++ src/modules/Content/templates/block/onepage_modify.tpl
@@ -0,0 +1,15 @@
+<div class="z-formrow">
+    <label for="Content_root">{gt text="Include the following page"}</label>
+    <select id="Content_root" name="root">
+        {foreach from=$pidItems item=item}
+        <option value="{$item.value}"{if $item.value eq $root} selected="selected"{/if}>
+            {$item.text}
+        </option>
+        {/foreach}
+    </select>
+</div>
+
+<div class="z-formrow">
+    <label for="Content_usecaching">{gt text="Use caching in the menu block"}</label>
+    <input id="Content_usecaching" type="checkbox" value="1" name="usecaching"{if $usecaching} checked="checked"{/if} />
+</div>
