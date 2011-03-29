f52532dc572075d49a8f11175a3df712dda4f382
Craig Heydenburg <craigh@mac.com>
2011-03-29 16:56:39 -0400

    fix missing refactor.

--- src/modules/Content/lib/Content/Controller/Admin.php
+++ src/modules/Content/lib/Content/Controller/Admin.php
@@ -7,7 +7,7 @@
  * @link http://code.zikula.org/content
  * @license See license.txt
  */
-class Content_Controller_Admin extends Zikula_Controller
+class Content_Controller_Admin extends Zikula_AbstractController
 {
 
     /**
