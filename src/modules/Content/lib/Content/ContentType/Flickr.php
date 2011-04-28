06d5bb180d03eb6e77d6d48ac6538ee5524eeafe
Craig Heydenburg <craigh@mac.com>
2011-04-28 16:55:58 -0400

    Flickr plugin fixes and code formatting. refs #180. still may (or may not) work. I can't seem to test it.

--- src/modules/Content/lib/Content/ContentType/Flickr.php
+++ src/modules/Content/lib/Content/ContentType/Flickr.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Content flickr plugin
  *
@@ -6,11 +7,11 @@
  * @link http://code.zikula.org/content
  * @license See license.txt
  */
-
 require_once 'modules/Content/lib/vendor/phpFlickr-3.1/phpFlickr.php';
 
 class Content_ContentType_Flickr extends Content_AbstractContentType
 {
+
     protected $userName;
     protected $tags;
     protected $photoCount;
@@ -49,48 +50,54 @@ class Content_ContentType_Flickr extends Content_AbstractContentType
     {
         return $this->__('Photos from Flickr.com');
     }
+
     function getDescription()
     {
         return $this->__('Display thumbnails from specific Flickr user or tags.');
     }
+
     function getAdminInfo()
     {
         return $this->__('If your want to add Flickr photos to your content then you need a Flickr API key. You can get this from <a href="http://www.flickr.com/api">flickr.com</a>.');
     }
+
     function isActive()
     {
-        $apiKey =  ModUtil::getVar('Content', 'flickrApiKey');
+        $apiKey = ModUtil::getVar('Content', 'flickrApiKey');
         if (!empty($apiKey)) {
             return true;
         }
         return false;
+        return true;
     }
+
     function loadData(&$data)
     {
         $this->userName = $data['userName'];
         $this->tags = $data['tags'];
         $this->photoCount = $data['photoCount'];
     }
+
     function display()
     {
-        $this->flickr = new phpFlickr($this->getVar('flickrApiKey'));
-        $this->flickr->enableCache("fs", System::getVar('temp'));
+        $flickr = new phpFlickr(ModUtil::getVar('Content', 'flickrApiKey'));
+        $flickr->enableCache("fs", System::getVar('temp'));
 
         // Find the NSID of the username
-        $person = $this->flickr->people_findByUsername($this->userName);
+        $person = $flickr->people_findByUsername($this->userName);
 
         // Get the photos
-        //$photos = $this->flickr->people_getPublicPhotos($person['id'], NULL, $this->photoCount);
-        $photos = $this->flickr->photos_search(array(
-            'user_id' => $person['id'],
-            'tags' => $this->tags,
-            'per_page' => $this->photoCount));
+        //$photos = $flickr->people_getPublicPhotos($person['id'], NULL, $this->photoCount);
+        $photos = $flickr->photos_search(array(
+                    'user_id' => $person['id'],
+                    'tags' => $this->tags,
+                    'per_page' => $this->photoCount));
 
         $photoData = array();
-        foreach ((array) $photos['photo'] as $photo) {
+        foreach ((array)$photos['photo'] as $photo) {
             $photoData[] = array(
                 'title' => DataUtil::formatForDisplayHTML($this->decode($photo['title'])),
-                'src' => $this->flickr->buildPhotoURL($photo, "Square"),
+                'src' => $flickr->buildPhotoURL($photo, "Square"),
                 'url' => "http://www.flickr.com/photos/$photo[owner]/$photo[id]");
         }
 
@@ -98,20 +105,25 @@ class Content_ContentType_Flickr extends Content_AbstractContentType
 
         return $this->view->fetch($this->getTemplate());
     }
+
     function displayEditing()
     {
         return $this->__f('Flickr photos from user %1$s associated with tags %2$s', array($this->userName, $this->tags));
     }
+
     function getDefaultData()
     {
         return array('userName' => '', 'tags' => '', 'photoCount' => 8);
     }
+
     function startEditing()
     {
-        $this->view->assign('flickrApiKey', $this->getVar('flickrApiKey'));
+        $this->view->assign('flickrApiKey', ModUtil::getVar('Content', 'flickrApiKey'));
     }
+
     function decode($s)
     {
         return mb_convert_encoding($s, mb_detect_encoding($s), 'UTF-8');
     }
+
 }
