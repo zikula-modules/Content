98d2bfe6835abab89066a1b0b805f6cc2b76de70
Axel Guckelsberger <info@guite.de>
2014-07-03 06:42:37 +0200

    Correct casing of getLinks method

--- lib/Content/Api/User.php
+++ lib/Content/Api/User.php
@@ -14,7 +14,7 @@ class Content_Api_User extends Zikula_AbstractApi
      *
      * @return array array of admin links
      */
-    public function getlinks()
+    public function getLinks()
     {
         $links = array();
 
