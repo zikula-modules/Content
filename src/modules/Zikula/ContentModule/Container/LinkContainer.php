6361540d6cf190a7d79ec7ec3627d9cbd500ed23
Guite <info@guite.de>
2018-06-14 14:27:38 +0200

    basic customisation of link container

--- src/modules/Zikula/ContentModule/Container/LinkContainer.php
+++ src/modules/Zikula/ContentModule/Container/LinkContainer.php
@@ -13,11 +13,109 @@
 namespace Zikula\ContentModule\Container;
 
 use Zikula\ContentModule\Container\Base\AbstractLinkContainer;
+use Zikula\Core\LinkContainer\LinkContainerInterface;
 
 /**
  * This is the link container service implementation class.
  */
 class LinkContainer extends AbstractLinkContainer
 {
-    // feel free to add own extensions here
+    /**
+     * @inheritDoc
+     */
+    public function getLinks($type = LinkContainerInterface::TYPE_ADMIN)
+    {
+        $links = parent::getLinks($type);
+        $hasEditPermissions = $this->permissionApi->hasPermission($this->getBundleName() . ':Page:', '::', ACCESS_EDIT);
+        $hasAddPermissions = $this->permissionApi->hasPermission($this->getBundleName() . ':Page:', '::', ACCESS_ADD);
+
+        $routeArea = LinkContainerInterface::TYPE_ADMIN == $type ? 'admin' : '';
+
+        // TODO properly add to tree... ModelHelper#canBeCreated returns false
+        $addNewPageLink = [
+            'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'edit'),
+            'text' => $this->__('Add a new page', 'zikulacontentmodule'),
+            'title' => $this->__('Add a new page', 'zikulacontentmodule'),
+            'icon' => 'plus-square fa-fw'
+        ];
+
+        if (LinkContainerInterface::TYPE_ACCOUNT == $type) {
+            foreach ($links as $k => $v) {
+                if ($v['icon'] == 'list-alt') {
+                    $links[$k]['icon'] = 'book';
+                }
+            }
+            if ($hasAddPermissions) {
+                if ($this->permissionApi->hasPermission($this->getBundleName() . '::', '::', ACCESS_ADMIN)) {
+                    // add link between the two existing ones
+                    $links = [
+                        $links[0],
+                        $addNewPageLink,
+                        $links[1]
+                    ];
+                } else {
+                    $links[] = $addNewPageLink;
+                }
+            }
+        } elseif (in_array($type, [LinkContainerInterface::TYPE_ADMIN, LinkContainerInterface::TYPE_USER])) {
+            // TODO
+            $pagesSubLinks = [];
+            if ($hasAddPermissions) {
+                $pagesSubLinks[] = $addNewPageLink;
+            }
+
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view'),
+                'text' => $this->__('Tabular view', 'zikulacontentmodule'),
+                'title' => $this->__('Shows the pages table', 'zikulacontentmodule'),
+                'icon' => 'table fa-fw'
+            ];
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view', ['tpl' => 'tree']),
+                'text' => $this->__('Hierarchy view', 'zikulacontentmodule'),
+                'title' => $this->__('Shows the pages tree', 'zikulacontentmodule'),
+                'icon' => 'code-fork fa-fw'
+            ];
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view', ['tpl' => 'sitemap']),
+                'text' => $this->__('Sitemap', 'zikulacontentmodule'),
+                'title' => $this->__('Sitemap', 'zikulacontentmodule'),
+                'icon' => 'sitemap fa-fw'
+            ];
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view', ['tpl' => 'extended']),
+                'text' => $this->__('Extended', 'zikulacontentmodule'),
+                'title' => $this->__('Extended page list (showing page headers)', 'zikulacontentmodule'),
+                'icon' => 'list fa-fw'
+            ];
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view', ['tpl' => 'complete']),
+                'text' => $this->__('Complete', 'zikulacontentmodule'),
+                'title' => $this->__('Complete page list (showing complete pages)', 'zikulacontentmodule'),
+                'icon' => 'th-large fa-fw'
+            ];
+            $pagesSubLinks[] = [
+                'url' => $this->router->generate('zikulacontentmodule_page_' . $routeArea . 'view', ['tpl' => 'categories']),
+                'text' => $this->__('Category list', 'zikulacontentmodule'),
+                'title' => $this->__('Show content by category', 'zikulacontentmodule'),
+                'icon' => 'archive fa-fw'
+            ];
+/** TODO probably unneeded because normal view shows link for viewing deleted pages if some exist
+            if ($routeArea == 'admin' && $hasEditPermissions) {
+                [
+                    'url'  => ModUtil::url('Content', 'admin', 'deletedpages'),
+                    'text' => $this->__('Restore pages')
+                ]
+            }
+*/
+            foreach ($links as $k => $v) {
+                if ($v['text'] == $this->__('Pages', 'zikulacontentmodule')) {
+                    $links[$k]['icon'] = 'book';
+                    $links[$k]['links'] = $pagesSubLinks;
+                }
+            }
+        }
+
+        return $links;
+    }
 }
