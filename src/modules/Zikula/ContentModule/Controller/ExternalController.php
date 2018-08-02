2354f04fd205fbb68ba6dfbcbc4190ae046ee4ed
Guite CI <support@guite.de>
2018-08-02 15:47:56 +0200

    Regenerated using latest generator

--- src/modules/Zikula/ContentModule/Controller/ExternalController.php
+++ src/modules/Zikula/ContentModule/Controller/ExternalController.php
@@ -23,21 +23,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExternalController extends AbstractExternalController
 {
     /**
-     * Displays one item of a certain object type using a separate template for external usages.
-     *
+     * @inheritDoc
      * @Route("/display/{objectType}/{id}/{source}/{displayMode}",
      *        requirements = {"id" = "\d+", "source" = "block|contentType|scribite", "displayMode" = "link|embed"},
      *        defaults = {"source" = "contentType", "displayMode" = "embed"},
      *        methods = {"GET"}
      * )
-     *
-     * @param Request $request     The current request
-     * @param string  $objectType  The currently treated object type
-     * @param int     $id          Identifier of the entity to be shown
-     * @param string  $source      Source of this call (block, contentType, scribite)
-     * @param string  $displayMode Display mode (link or embed)
-     *
-     * @return string Desired data output
      */
     public function displayAction(Request $request, $objectType, $id, $source, $displayMode)
     {
@@ -45,27 +36,13 @@ class ExternalController extends AbstractExternalController
     }
 
     /**
-     * Popup selector for Scribite plugins.
-     * Finds items of a certain object type.
-     *
+     * @inheritDoc
      * @Route("/finder/{objectType}/{editor}/{sort}/{sortdir}/{pos}/{num}",
      *        requirements = {"editor" = "ckeditor|quill|summernote|tinymce", "sortdir" = "asc|desc", "pos" = "\d+", "num" = "\d+"},
      *        defaults = {"sort" = "", "sortdir" = "asc", "pos" = 1, "num" = 0},
      *        methods = {"GET"},
      *        options={"expose"=true}
      * )
-     *
-     * @param Request $request    The current request
-     * @param string  $objectType The object type
-     * @param string  $editor     Name of used Scribite editor
-     * @param string  $sort       Sorting field
-     * @param string  $sortdir    Sorting direction
-     * @param int     $pos        Current pager position
-     * @param int     $num        Amount of entries to display
-     *
-     * @return output The external item finder page
-     *
-     * @throws AccessDeniedException Thrown if the user doesn't have required permissions
      */
     public function finderAction(Request $request, $objectType, $editor, $sort, $sortdir, $pos = 1, $num = 0)
     {
