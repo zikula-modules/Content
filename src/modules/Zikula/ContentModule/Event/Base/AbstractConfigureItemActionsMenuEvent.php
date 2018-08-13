ce1609fbb9471bd84cb45554891c8b7986ba86bf
Guite CI <support@guite.de>
2018-08-13 15:59:28 +0200

    Regenerated using latest generator

--- src/modules/Zikula/ContentModule/Event/Base/AbstractConfigureItemActionsMenuEvent.php
+++ src/modules/Zikula/ContentModule/Event/Base/AbstractConfigureItemActionsMenuEvent.php
@@ -31,15 +31,22 @@ class AbstractConfigureItemActionsMenuEvent extends Event
     protected $menu;
 
     /**
+     * @var array
+     */
+    protected $options;
+
+    /**
      * ConfigureItemActionsMenuEvent constructor.
      *
      * @param FactoryInterface $factory
      * @param ItemInterface    $menu
+     * @param array            $options
      */
-    public function __construct(FactoryInterface $factory, ItemInterface $menu)
+    public function __construct(FactoryInterface $factory, ItemInterface $menu, array $options = [])
     {
         $this->factory = $factory;
         $this->menu = $menu;
+        $this->options = $options;
     }
 
     /**
@@ -60,5 +67,15 @@ class AbstractConfigureItemActionsMenuEvent extends Event
     public function getMenu()
     {
         return $this->menu;
+    }
+
+    /**
+     * Returns the options.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
     }
 }
