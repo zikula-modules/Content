181267ad69ca5515b74e6df3654cd8ae63633f04
Guite CI <support@guite.de>
2018-08-18 12:24:21 +0200

    Regenerated using latest generator

--- src/modules/Zikula/ContentModule/Block/Base/AbstractItemListBlock.php
+++ src/modules/Zikula/ContentModule/Block/Base/AbstractItemListBlock.php
@@ -47,7 +47,7 @@ abstract class AbstractItemListBlock extends AbstractBlockHandler
      */
     public function getType()
     {
-        return $this->__('List of content items', 'zikulacontentmodule');
+        return $this->__('Content list', 'zikulacontentmodule');
     }
     
     /**
@@ -110,7 +110,7 @@ abstract class AbstractItemListBlock extends AbstractBlockHandler
     
         // set a block title
         if (empty($properties['title'])) {
-            $properties['title'] = $this->__('ZikulaContentModule items', 'zikulacontentmodule');
+            $properties['title'] = $this->__('Content list', 'zikulacontentmodule');
         }
     
         $template = $this->getDisplayTemplate($properties);
