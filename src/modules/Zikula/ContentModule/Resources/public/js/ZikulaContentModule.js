1dbdb2e66c8752959ea2b5a920159d1f3c21a9fe
Guite CI <support@guite.de>
2018-08-25 20:03:33 +0200

    Regenerated using latest generator

--- src/modules/Zikula/ContentModule/Resources/public/js/ZikulaContentModule.js
+++ src/modules/Zikula/ContentModule/Resources/public/js/ZikulaContentModule.js
@@ -148,7 +148,9 @@ function zikulaContentInitItemActions(context) {
 }
 
 /**
- * Helper function to create new Bootstrap modal window instances.
+ * Helper function to create new dialog window instances.
+ * Note we use jQuery UI dialogs instead of Bootstrap modals here
+ * because we want to be able to open multiple windows simultaneously.
  */
 function zikulaContentInitInlineWindow(containerElem) {
     var newWindowId;
