d0903a966dd2ff92d533f6aaa66d8773704792be
Guite <info@guite.de>
2018-08-29 13:56:01 +0200

    removed unneeded property

--- src/modules/Zikula/ContentModule/Form/Type/ContentItemType.php
+++ src/modules/Zikula/ContentModule/Form/Type/ContentItemType.php
@@ -32,11 +32,6 @@ class ContentItemType extends AbstractType
     use TranslatorTrait;
 
     /**
-     * @var TranslatorInterface
-     */
-    private $translator;
-
-    /**
      * @var ListEntriesHelper
      */
     private $listHelper;
