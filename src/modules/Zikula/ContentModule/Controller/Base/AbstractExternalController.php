881919b761f5f54ffae3ab23aacc5ba0c6d5e75a
Guite CI <support@guite.de>
2018-09-30 13:15:42 +0200

    Regenerated using latest generator

--- src/modules/Zikula/ContentModule/Controller/Base/AbstractExternalController.php
+++ src/modules/Zikula/ContentModule/Controller/Base/AbstractExternalController.php
@@ -167,7 +167,7 @@ abstract class AbstractExternalController extends AbstractController
         
         $qb = $repository->getListQueryBuilder($where, $orderBy);
         
-        if ($searchTerm != '') {
+        if ('' != $searchTerm) {
             $qb = $this->get('zikula_content_module.collection_filter_helper')->addSearchFilter($objectType, $qb, $searchTerm);
         }
         $query = $repository->getQueryFromBuilder($qb);
