403d08e58614e8890df2ba43df3e845d931045d1
Guite CI <support@guite.de>
2018-10-02 08:47:34 +0200

    Regenerated using latest generator

---
+++ src/modules/Zikula/ContentModule/Resources/public/scribite/Summernote/zikulacontentmodule/plugin.js
@@ -0,0 +1,30 @@
+( function ($) {
+    $.extend($.summernote.plugins, {
+        /**
+         * @param {Object} context - context object has status of editor.
+         */
+        'zikulacontentmodule': function (context) {
+            var self = this;
+
+            // ui provides methods to build ui elements.
+            var ui = $.summernote.ui;
+
+            // add button
+            context.memo('button.zikulacontentmodule', function () {
+                // create button
+                var button = ui.button({
+                    contents: '<img src="' + Zikula.Config.baseURL + Zikula.Config.baseURI + '/web/modules/zikulacontent/images/admin.png' + '" alt="Content" width="16" height="16" />',
+                    tooltip: 'Content',
+                    click: function () {
+                        ZikulaContentModuleFinderOpenPopup(context, 'summernote');
+                    }
+                });
+
+                // create jQuery object from button instance.
+                var $button = button.render();
+
+                return $button;
+            });
+        }
+    });
+})(jQuery);
---
+++ src/modules/Zikula/ContentModule/Resources/public/scribite/integration.md
@@ -0,0 +1,5 @@
+# SCRIBITE INTEGRATION
+
+It is easy to include ZikulaContentModule in your [Scribite editors](https://github.com/zikula-modules/Scribite/).
+ZikulaContentModule contains already a popup for selecting pages and other items.
+Please note that Scribite 6.0+ is required for this.
