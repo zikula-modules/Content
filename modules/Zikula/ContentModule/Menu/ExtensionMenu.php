6692393eaf833879cad793cc5e67f95ab79d78d7
Guite <info@guite.de>
2020-01-21 13:48:42 +0100

    restored custom class

--- modules/Zikula/ContentModule/Menu/ExtensionMenu.php
+++ modules/Zikula/ContentModule/Menu/ExtensionMenu.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace Zikula\ContentModule\Menu;
 
+use Knp\Menu\ItemInterface;
 use Zikula\ContentModule\Menu\Base\AbstractExtensionMenu;
 
 /**
@@ -21,5 +22,112 @@ use Zikula\ContentModule\Menu\Base\AbstractExtensionMenu;
  */
 class ExtensionMenu extends AbstractExtensionMenu
 {
-    // feel free to add own extensions here
+    public function get(string $type = self::TYPE_ADMIN): ?ItemInterface
+    {
+        $menu = parent::get($type);
+        $hasAddPermissions = $this->permissionHelper->hasComponentPermission('page', ACCESS_ADD);
+
+        $routeArea = self::TYPE_ADMIN === $type ? 'admin' : '';
+
+        if (self::TYPE_ACCOUNT === $type) {
+            $myPagesLink = $menu->getChild('My pages');
+            if (null !== $myPagesLink) {
+                $myPagesLink->setAttribute('icon', 'fas fa-book');
+            }
+            if ($hasAddPermissions) {
+                $menu->addChild('Add a new page', [
+                    'route' => 'zikulacontentmodule_page_' . $routeArea . 'edit'
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-plus-square')
+                ;
+
+                if ($this->permissionHelper->hasPermission(ACCESS_ADMIN)) {
+                    // remove and backend link and readd it after the "add a new page" link
+                    $backendLabel = 'Content Backend';
+                    $backendLink = $menu->getChild($backendLabel);
+                    if (null !== $backendLink) {
+                        $menu->removeChild($backendLabel);
+                        $menu->addChild('Content Backend', [
+                            'route' => 'zikulacontentmodule_page_adminindex'
+                        ])
+                            ->setAttribute('icon', 'fas fa-wrench')
+                        ;
+                    }
+                }
+            }
+
+            return 0 === $menu->count() ? null : $menu;
+        }
+
+        $pagesLink = $menu->getChild('Pages');
+        $pagesLink->setAttribute('icon', 'fas fa-book');
+        if (in_array($type, [self::TYPE_ADMIN, self::TYPE_USER], true)) {
+            $pagesLink->setAttribute('dropdown', true);
+            $viewRouteName = 'zikulacontentmodule_page_' . $routeArea . 'view';
+            if ('admin' === $routeArea) {
+                if ($hasAddPermissions) {
+                    $pagesLink->addChild('Add a new page', [
+                        'route' => 'zikulacontentmodule_page_' . $routeArea . 'edit'
+                    ])
+                        ->setAttribute('icon', 'fas fa-fw fa-plus-square')
+                    ;
+                }
+                $pagesLink->addChild('Tabular view', [
+                    'route' => $viewRouteName
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-table')
+                    ->setLinkAttribute('title', 'Shows the pages table')
+                ;
+                $pagesLink->addChild('Hierarchy view', [
+                    'route' => $viewRouteName,
+                    'routeParameters' => ['tpl' => 'tree']
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-code-branch')
+                    ->setLinkAttribute('title', 'Shows the pages tree')
+                ;
+            } else {
+                $pagesLink->addChild('Sitemap', [
+                    'route' => 'zikulacontentmodule_page_sitemap'
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-sitemap')
+                ;
+                if ($hasAddPermissions) {
+                    $pagesLink->addChild('Add a new page', [
+                        'route' => 'zikulacontentmodule_page_' . $routeArea . 'edit'
+                    ])
+                        ->setAttribute('icon', 'fas fa-fw fa-plus-square')
+                    ;
+                }
+                $pagesLink->addChild('Simple list', [
+                    'route' => $viewRouteName
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-table')
+                    ->setLinkAttribute('title', 'Shows a simple list of pages')
+                ;
+                $pagesLink->addChild('Extended list', [
+                    'route' => $viewRouteName,
+                    'routeParameters' => ['list' => 'extended']
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-list')
+                    ->setLinkAttribute('title', 'Shows an extended list of pages with first content elements')
+                ;
+                $pagesLink->addChild('Complete list', [
+                    'route' => $viewRouteName,
+                    'routeParameters' => ['list' => 'complete']
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-th-large')
+                    ->setLinkAttribute('title', 'Shows a complete list of pages with complete content')
+                ;
+                $pagesLink->addChild('Categories list', [
+                    'route' => $viewRouteName,
+                    'routeParameters' => ['list' => 'categories']
+                ])
+                    ->setAttribute('icon', 'fas fa-fw fa-archive')
+                    ->setLinkAttribute('title', 'Shows content grouped by categories')
+                ;
+            }
+        }
+
+        return 0 === $menu->count() ? null : $menu;
+    }
 }
